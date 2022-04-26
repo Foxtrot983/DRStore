d99a330e88ad55e891005ef0e371ebb1f56bdffe
Foxtrot983 <wladiksan@gmail.com>
2022-04-26 22:03:16 +0300

    securecode deleted

--- FWBack/FWBack/settings.py
+++ FWBack/FWBack/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-cv!9ob90do=m_b5m86y%+r5sh@ej_23yfrqvrdd@b(y%ch$ng+'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
